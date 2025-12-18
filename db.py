f1d7f4129648d4a95250706ea6d24649264007d1
Alejandro Sierra <devasierrav@gmail.com>
2025-12-18 11:19:14 -0500

    Adding credentials for DB

--- db.py
+++ db.py
@@ -2,8 +2,10 @@ import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
+        # host="localhost",
+        host="dpg-d522h03e5dus73akfcog-a",
+        database="inventario_pymes",
         user="root",
-        password="admin",
-        database="inventario_pymes"
+        password="LfVEm3H3d4E999WvdEAME1WrTJla5PZS",
+        port=5432
     )
